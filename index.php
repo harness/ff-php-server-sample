3e6662d659f2bc8330aa139c91228c88856e10cf
bmjen <bryan.jen@harness.io>
2022-06-27 11:20:56 -0700

    fix: Update class to reflect latest SDK changes

--- index.php
+++ index.php
@@ -2,16 +2,14 @@
 
 require_once realpath("vendor/autoload.php");
 
-use Harness\CFClient;
+use Harness\Client;
 use OpenAPI\Client\Model\Target;
 
 $SDK_KEY = getenv("SDK_KEY") ?: "";  // you can put your key in env variable or you can provide in the code
 
-$cfClient = new CFClient($SDK_KEY, new Target(["name" => "harness", "identifier" => "harness"]), [
+$cfClient = new Client($SDK_KEY, new Target(["name" => "harness", "identifier" => "harness"]), [
     "base_url" => "http://ff-proxy:7000",
     "events_url" => "http://ff-proxy:7000"
 ]);
 
 echo "Evaluation value for flag 'flag1' with target 'enver': " . $cfClient->evaluate("flag1", false);
-
-$cfClient->close();
